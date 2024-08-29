39aa10921e16e9dd8c9a7bd5cf665bc2e558f661
Christian Carrillo <christiancazu@gmail.com>
2024-08-29 00:23:52 -0500

    app: init setup

---
+++ apps/squadmakers-app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import 'christiancazu-squadmakers-lib/style.css'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">
+          Home
+        </RouterLink>
+        <RouterLink to="/about">
+          About
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
